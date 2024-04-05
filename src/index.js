1fbbb146a75eeb3e91ad4dd3019877eb49848db0
Julian Martin <julianmartin19@outlook.com>
2024-04-05 13:11:59 -0300

    estructura back

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import { PORT } from './config/config';
+import morgan from 'morgan';
+import cors from 'cors'
+// import { PORT } from './config/config.js';
+// import TestRoutes from './routes/test.routes.js'
 
 const app = express()
 
-app.listen(PORT, async()=>{
+app.use(morgan("dev")) //da mas informacion de la solicitud.
+app.use(cors()) //para que no revoten las peticiones.
 
-  console.log("La app esta escucnado el puerto PORT")
+// app.use(TestRoutes)
 
-})
+
+
+// app.listen(PORT, async()=>{
+//   console.log("La app esta escucnado el puerto PORT")
+
+// })
+// app.listen(4000, async()=>{
+//   console.log("La app esta escuchando el puerto 4000")
+// })
+
+// app.get("/", async(req,res)=>{
+//   res.send("Hello World")
+// })
---
+++ src/routes/test.routes.js
@@ -0,0 +1,9 @@
+// import { Router } from "express";
+
+// const router = Router()
+
+// router.get("/test", async(req,res)=>{
+//   res.send("Endpoint de test")
+// })
+
+// export default router;
