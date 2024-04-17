2feb6da4ed2de6f444e6947862b8f529643d02d7
Alvaro D. <alvarodeguer@gmail.com>
2024-04-16 22:39:42 -0300

    Ajustado modelo con requerimientos

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,38 +5,42 @@ const productSchema = new Schema(
   {
     name: {
       type: String,
-      // required: true,
-      // minLength: [3, 'the field name must have at least 3 characters'],
-      // maxLength: [100, 'the field name must have a maximun of 100 characters'],
+      required: [true, "The field name is required"],
+      minLength: [3, 'the field name must have at least 3 characters'],
+      maxLength: [100, 'the field name must have a maximun of 100 characters'],
     },
     description: {
       type: String,
-      // required: true,
-      // minLength: [3, 'the description field must have at least 3 characters'],
-      // maxLength: [
-      //   500,
-      //   'the description field must have a maximun of 500 characters',
-      // ]
+      required: [true, "The field description is required"],
+      minLength: [3, 'the description field must have at least 3 characters'],
+      maxLength: [500,'the description field must have a maximun of 500 characters'],
     },
-		// category: {
-		// 	type: Schema.Types.ObjectId,
-		// 	ref: 'Category',
-		// 	required: true,
-		// },
-		price: {
-			type: Number,
-			// required: true,
-			// min: [1, 'The min price is 1 , and you enter ${VALUE}'],
-			default: 1,
-		},
-		image: {
-			type: String,
-			match: [
-				/^.*\.(jpg|jpeg|png|gif|bmp)$/i,
-				'Please provide a valid image path',
-			],
-			default: 'https://i.imgur.com/I03y2Ec.png',
-		},
+	category: {
+		type: String,
+        required: [true, "The field category is required"],
+        enum : ["Bebidas", "Herramientas", "Limpieza", "Otros"], 
+	},
+	price: {
+		type: Number,
+		required: true,
+		min: 0,
+		default: 0,
+		
+	},
+	stock: {
+		type: Number,
+		required: true,
+		min: 0,
+		default: 0,
+	},
+	image: {
+		type: String,
+		match: [
+			/^.*\.(jpg|jpeg|png|gif|bmp)$/i,
+			'Please provide a valid image path',
+		],
+		default: 'age-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg',
+	},
 	},
 	{
 		timestamps: true,
