776cc5fe3def3a462cf666856aa20bd1cd705d78
ignacio skib <nanoskibski@gmail.com>
2024-04-23 20:52:47 -0300

    arreglando el id

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,6 +3,10 @@ import { Schema, model } from 'mongoose'
 const productSchema = new Schema(
 
   {
+	_id: {
+		type: String,
+	  required: true,
+	  },
     name: {
       type: String,
     required: true,
