4283fbae916d73f9a36ae3d874e44a533661eee9
Alvaro D. <alvarodeguer@gmail.com>
2024-04-23 00:10:29 -0300

    Mejorado el modal de validaciones

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,37 +5,34 @@ const productSchema = new Schema(
   {
     name: {
       type: String,
-    required: true,
-       minLength: [3, 'the field name must have at least 3 characters'],
-       maxLength: [100, 'the field name must have a maximun of 100 characters'],
+	  required: [true, "The field name is required"],
+      minLength: [2, 'the field name must have at least 3 characters'],
+      maxLength: [100, 'the field name must have a maximun of 100 characters'],
     },
     description: {
-      type: String,
-      required: true,
-       minLength: [3, 'the description field must have at least 3 characters'],
-       maxLength: [
-         500,
-        'the description field must have a maximun of 500 characters',
-      ]
+		type: String,
+		required: true,
+		minLength: [2, 'the description field must have at least 3 characters'],
+		maxLength: [500,'the description field must have a maximun of 500 characters']
     },
 		category: {
 			type: String,
-		// 	ref: 'Category',
-		 	required: true,
+			required: [true, "The field category is required"],
+			enum : ["Bebidas", "Herramientas", "Limpieza", "Otros"],
 		},
 		price: {
 			type: Number,
 			required: true,
-			 min: 1,
-			 max: 1_000_000,
-			default: 1,
+			min: 0,
+			max: 1_000_000,
+			default: 0,
 		},
 		stock: {
 			type: Number,
 			required: true,
-			 min: 1,
-			 max: 1_000_000,
-			default: 1,
+			min: 1,
+			max: 1_000_000,
+			default: 0,
 		},
 		image: {
 			type: String,
