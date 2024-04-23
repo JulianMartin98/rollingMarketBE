5f502b9a199b20cd82685ea2911198977e6371e8
Alvaro D. <alvarodeguer@gmail.com>
2024-04-23 00:19:44 -0300

    Limpiando el codigo y mejorando el update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,6 @@
 import { ProductModel } from "../models/Product.js"
-import { productos } from "../db/db.js"
-
 
 export async function GetAllProducts  (req, res)  {
-  //res.status(200).json(productos)
   try {
     
     const products = await ProductModel.find()
@@ -11,14 +8,12 @@ export async function GetAllProducts  (req, res)  {
 
 
   } catch (error) {
-    console.log("error")
+    console.log(error)
   }
 };
 
 
 export const CreateProduct = async (req, res) => {
-  //console.log(req.body)
-  //const {nombre, descripcion, categoria, precio, imagen} = req.body
   try {
   const body = await req.body
   console.log("----BODY----", body);
@@ -29,39 +24,18 @@ export const CreateProduct = async (req, res) => {
   } catch (error) {
     console.log(error)
   }
-//try {
-  //const newProduct = await productos.create({
-    //nombre: nombre,
-    //descripcion: descripcion,
-    //categoria: categoria,
-    //precio: precio,
-    //imagen: imagen,
-  //})
-  //res.status(201).json({message: "el producto se ha creado con exito"})
-//} catch (error) {
-  //return res.status(500).json({message: error})
-//}
 
 }
 
 export async function GetProductById (req, res)  {
   const { id } = req.params
-  //const productFound = productos.find(product => product.id === id)
-
-  //if(productFound) {
-  //  return res.status(200).json(productFound)
-  //}
-  //res.status(404).json({message: "no hemos podido encontrar los productos"})
-  
-  //console.log(productFound)
-  //console.log(id)
   try {
     
     const product = await ProductModel.find({_id:id})
     return res.status(200).json(product)
 
   } catch (error) {
-   console.log("error")
+   console.log(error)
   }
 };
 
@@ -71,7 +45,6 @@ export async function DeleteProduct (req, res)  {
     const product = await ProductModel.deleteOne({_id:id})
     return res.status(200).json("producto borrado exitosamente")
 
-
   } catch (error) {
     console.log(error)
   }
@@ -80,15 +53,14 @@ export async function DeleteProduct (req, res)  {
 export async function UpdateProduct (req, res)  {
   try {
     const {id} = req.params 
-    const {name,description} = req.body
-    const product = await ProductModel.findOneAndUpdate({_id:id},{name,description})
+    const {name,description,category,price,stock,image} = req.body
+    const product = await ProductModel.findOneAndUpdate({_id:id},{name,description,category,price,stock,image})
     if(!product){
       return res.status(404).json("producto no encontrado")
     }
      return res.status(200).json("producto actualizado exitosamente")
 
-
-  } catch (error) {
+    } catch (error) {
     console.log(error)
   }
 };
