bf7550c73fbe5c7d83d242e03e542f2f4f5dfb95
Julian Martin <julianmartin19@outlook.com>
2024-04-23 23:32:38 -0300

    cambiando confg de tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateUser, DeleteUser, GetAllUsers, GetUserById, UpdateUser, LoginUser } from "../controllers/user.controllers.js";
+import comprobarJWT from '../middlewares/verifyJWT.js';
+
+
 
 const router = Router()
 
-router.get("/usuarios",GetAllUsers);
+router.get("/usuarios",comprobarJWT,GetAllUsers);
 router.post("/usuario",CreateUser);
-router.get("/usuario/:id",GetUserById);
-router.delete("/usuario/delete/:id",DeleteUser);
-router.put("/usuario/update/:id",UpdateUser);
+router.get("/usuario/:id",comprobarJWT,GetUserById);
+router.delete("/usuario/delete/:id",comprobarJWT,DeleteUser);
+router.put("/usuario/update/:id",comprobarJWT,UpdateUser);
 router.post("/login/",LoginUser);
 
 
